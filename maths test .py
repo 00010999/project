0792a18c608bb000d1742a2693310724b86fc2ce
00010999 <95466035+00010999@users.noreply.github.com>
2021-12-03 12:28:52 +0530

    Add files via upload

---
+++ maths test .py
@@ -0,0 +1,13 @@
+
+
+
+print("welcome to the test ")
+print("Queston 1 : what is 5 plus 6")
+a = input("no")
+a= int (a)
+b = 11
+int (b)
+if a==b :
+    print("correct")
+if a>b :
+    print("wrong")
